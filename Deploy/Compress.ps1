ecb4cb23b1518f3f32d10772c963552f3afbf8c6
Hexah <hexer504@gmail.com>
2020-04-10 21:19:01 +0200

    Update Compress.ps1

--- Deploy/Compress.ps1
+++ Deploy/Compress.ps1
@@ -1,3 +1,5 @@
+$loc = Get-Location
+Set-Location 'bin\Release\'
 $compress = @{
 LiteralPath= "sourcepawn/", "Spedit.exe", "MahApps.Metro.dll", "ICSharpCode.AvalonEdit.dll", "System.Windows.Interactivity.dll", "Xceed.Wpf.AvalonDock.dll", "Xceed.Wpf.AvalonDock.Themes.Metro.dll", "smxdasm.dll", "LysisForSpedit.dll", "QueryMaster.dll", "Ionic.BZip2.dll", "SourcepawnCondenser.dll", "Renci.SshNet.dll", "Newtonsoft.Json.dll", "DiscordRPC.dll", "ControlzEx.dll", "lang_0_spedit.xml", "GPLv3.txt"
 #Path= "sourcepawn/"
@@ -6,3 +8,4 @@ DestinationPath = "SPEdit.Portable.zip"
 
 }
 Compress-Archive -Force @compress
+Set-Location $loc
