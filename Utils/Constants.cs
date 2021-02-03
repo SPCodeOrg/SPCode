1312dca490c1f3469a23591d377d7fe65be7d46f
Maxi Jabase <maxijabase@gmail.com>
2021-02-03 13:31:42 -0300

    moved the RPC App ID to constants
     (im sorry this is the last one)

--- Utils/Constants.cs
+++ Utils/Constants.cs
@@ -3,5 +3,6 @@
     public class Constants
     {
         public static string GitHubNewIssueLink = "https://github.com/Hexer10/SPCode/issues/new";
+        public static string DiscordRPCAppID = "692110664948514836";
     }
 }
