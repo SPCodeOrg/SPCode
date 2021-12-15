df841b2dab524fccd7ba13641e323dd54d984a25
Maxi Jabase <maxijabase@gmail.com>
2021-12-15 17:44:16 -0300

    made postbuild file

--- Deploy/prebuild.bat
+++ Deploy/prebuild.bat
@@ -1,21 +1,16 @@
+echo --------------------------
+echo **************************
+echo BEGIN PREBUILD
+echo **************************
+echo --------------------------
+
 REM // fetch git count for beta rev
 for /f "tokens=*" %%A in ('git rev-list HEAD --count') do set "REV=%%A"
 
-echo ****************************************
-echo Directorio: %cd%
-echo ****************************************
-
 REM // fetch latest tag
 for /f "tokens=*" %%B in ('git describe --tags --abbrev^=0') do set "TAG=%%B"
-
-echo ****************************************
-echo REVISION: %REV%
-echo TAG: %TAG%
-echo ****************************************
 
 REM // make assemblyinfo from template
 envsubst -i AssemblyInfo_Template.cs -o ..\App\AssemblyInfo.cs
 
 exit 0
-
-
