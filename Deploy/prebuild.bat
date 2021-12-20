98e56d6bbe7a94f8b3169af784e89eb4c24ecb0c
Maxi Jabase <maxijabase@gmail.com>
2021-12-20 09:28:52 -0300

    Update prebuild.bat

--- Deploy/prebuild.bat
+++ Deploy/prebuild.bat
@@ -8,7 +8,7 @@ REM // fetch git count for beta rev
 for /f "tokens=*" %%A in ('git rev-list HEAD --count') do set "REV=%%A"
 
 REM // fetch latest tag
-for /f "tokens=*" %%B in ('git describe --tags --abbrev^=0') do set "TAG=%%B"
+for /f "tokens=*" %%B in ('git describe --match "[0-9].[0-9].[0-9].[0-9]" --tags --abbrev^=0') do set "TAG=%%B"
 
 REM // make assemblyinfo from template
 envsubst -i AssemblyInfo_Template.cs -o ..\App\AssemblyInfo.cs
