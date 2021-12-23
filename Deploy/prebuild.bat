52e66780fbbb496c675c44e511c6e65eccbff6cc
Maxi Jabase <maxijabase@gmail.com>
2021-12-23 20:10:25 -0300

    fixed route in prebuild

--- Deploy/prebuild.bat
+++ Deploy/prebuild.bat
@@ -14,15 +14,13 @@ if "%1"=="Debug-Beta" set beta=1
 if "%1"=="Release-Beta" set beta=1
 
 if %beta%==1 (
-	echo "aaaa"
 	copy "..\Resources\Icons\IconTemplates\Icon_Beta.ico" "..\Resources\Icons\Icon.ico" /y
 	copy "..\Resources\Icons\IconTemplates\icon256xbeta.png" "..\Resources\Icons\icon256x.png" /y
-	copy ".\..\..\Deploy\icon_beta.ico" ".\icon.ico" /y
+	copy "..\Deploy\icon_beta.ico" ".\icon.ico" /y
 ) else (
-	echo "bbbb"
 	copy "..\Resources\Icons\IconTemplates\Icon.ico" "..\Resources\Icons\Icon.ico" /y
 	copy "..\Resources\Icons\IconTemplates\icon256x.png" "..\Resources\Icons\icon256x.png" /y
-	copy ".\..\..\Deploy\icon_stable.ico" ".\icon.ico" /y
+	copy "..\Deploy\icon_stable.ico" ".\icon.ico" /y
 )
 
 exit 0
