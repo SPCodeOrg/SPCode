37d0a8fb04d731f64e276b089d58b7900b33bc52
Maxi Jabase <maxijabase@gmail.com>
2022-02-14 11:55:00 -0300

    finish rewriting configs window UI

--- UI/Windows/OptionsWindow/OptionsWindowHighlighting.cs
+++ UI/Windows/OptionsWindow/OptionsWindowHighlighting.cs
@@ -26,90 +26,90 @@ namespace SPCode.UI.Windows
 
         private void Comments_Changed(object sender, RoutedEventArgs e)
         {
-            if (!AllowChanging) { return; }
+            if (!AllowChanging) return;
             Program.OptionsObject.SH_Comments = SH_Comments.GetColor();
         }
 
         private void CommentMarker_Changed(object sender, RoutedEventArgs e)
         {
-            if (!AllowChanging) { return; }
+            if (!AllowChanging) return;
             Program.OptionsObject.SH_CommentsMarker = SH_CommentMarkers.GetColor();
         }
 
         private void PreProcessor_Changed(object sender, RoutedEventArgs e)
         {
-            if (!AllowChanging) { return; }
+            if (!AllowChanging) return;
             Program.OptionsObject.SH_PreProcessor = SH_PreProcessor.GetColor();
         }
 
         private void String_Changed(object sender, RoutedEventArgs e)
         {
-            if (!AllowChanging) { return; }
+            if (!AllowChanging) return;
             Program.OptionsObject.SH_Strings = SH_Strings.GetColor();
         }
 
         private void Types_Changed(object sender, RoutedEventArgs e)
         {
-            if (!AllowChanging) { return; }
+            if (!AllowChanging) return;
             Program.OptionsObject.SH_Types = SH_Types.GetColor();
         }
 
         private void TypeValues_Changed(object sender, RoutedEventArgs e)
         {
-            if (!AllowChanging) { return; }
+            if (!AllowChanging) return;
             Program.OptionsObject.SH_TypesValues = SH_TypesValues.GetColor();
         }
 
         private void Keywords_Changed(object sender, RoutedEventArgs e)
         {
-            if (!AllowChanging) { return; }
+            if (!AllowChanging) return;
             Program.OptionsObject.SH_Keywords = SH_Keywords.GetColor();
         }
 
         private void ContextKeywords_Changed(object sender, RoutedEventArgs e)
         {
-            if (!AllowChanging) { return; }
+            if (!AllowChanging) return;
             Program.OptionsObject.SH_ContextKeywords = SH_ContextKeywords.GetColor();
         }
 
         private void Chars_Changed(object sender, RoutedEventArgs e)
         {
-            if (!AllowChanging) { return; }
+            if (!AllowChanging) return;
             Program.OptionsObject.SH_Chars = SH_Chars.GetColor();
         }
         private void UFunctions_Changed(object sender, RoutedEventArgs e)
         {
-            if (!AllowChanging) { return; }
+            if (!AllowChanging) return;
             Program.OptionsObject.SH_UnkownFunctions = SH_UnknownFunctions.GetColor();
         }
         private void Numbers_Changed(object sender, RoutedEventArgs e)
         {
-            if (!AllowChanging) { return; }
+            if (!AllowChanging) return;
             Program.OptionsObject.SH_Numbers = SH_Numbers.GetColor();
         }
         private void SpecialCharacters_Changed(object sender, RoutedEventArgs e)
         {
-            if (!AllowChanging) { return; }
+            if (!AllowChanging) return;
             Program.OptionsObject.SH_SpecialCharacters = SH_SpecialCharacters.GetColor();
         }
         private void Deprecated_Changed(object sender, RoutedEventArgs e)
         {
-            if (!AllowChanging) { return; }
+            if (!AllowChanging) return;
             Program.OptionsObject.SH_Deprecated = SH_Deprecated.GetColor();
         }
         private void Constants_Changed(object sender, RoutedEventArgs e)
         {
-            if (!AllowChanging) { return; }
+            if (!AllowChanging) return;
             Program.OptionsObject.SH_Constants = SH_Constants.GetColor();
         }
         private void Functions_Changed(object sender, RoutedEventArgs e)
         {
-            if (!AllowChanging) { return; }
+            if (!AllowChanging) return;
             Program.OptionsObject.SH_Functions = SH_Functions.GetColor();
         }
         private void Methods_Changed(object sender, RoutedEventArgs e)
         {
-            if (!AllowChanging) { return; }
+            if (!AllowChanging) return;
             Program.OptionsObject.SH_Methods = SH_Methods.GetColor();
         }
     }
